2f1bc80747ca8b5892e03f4009b4fb0a0dd644a8
Kraypex <89335118+Kraypex@users.noreply.github.com>
2021-11-02 15:42:22 +0530

    Add files via upload

---
+++ Exporter Options/update.py
@@ -0,0 +1,30 @@
+import json
+import colorama
+import requests
+from time import sleep
+import sys
+
+from colorama import Fore
+
+colorama.init(autoreset=True)
+
+def update():
+    assets = requests.get('https://benbot.app/api/v1/files/added').json()
+    aes = requests.get('https://benbot.app/api/v1/status').json()
+    with open('datas/assets.json', 'w') as file:
+        json.dump(assets, file, indent=2)
+    with open('datas/Pakchunks.json', 'w') as file:
+        json.dump(aes, file, indent=2)
+
+ask = input('Do you want to update the json files?\n(1) Yes\n(2) No\n->>> ')
+if ask == '1':
+    assets = requests.get('https://benbot.app/api/v1/files/added').json()
+    aes = requests.get('https://benbot.app/api/v1/status').json()
+    with open('datas/assets.json', 'w') as file:
+        json.dump(assets, file, indent=2)
+    with open('datas/Pakchunks.json', 'w') as file:
+        json.dump(aes, file, indent=2)
+        print(Fore.GREEN + f'\nUpdated successfully\n')
+        sleep(1)
+if ask == '2':
+    pass                                         
