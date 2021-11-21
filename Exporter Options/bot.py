eb0fd4c8bedb21a1044b1f282d8f3d491c3f260b
Kraypex <89335118+Kraypex@users.noreply.github.com>
2021-11-21 16:02:06 +0530

    Updated bot.py

--- Exporter Options/bot.py
+++ Exporter Options/bot.py
@@ -3,12 +3,10 @@ import requests
 import colorama
 import time
 from time import sleep
-import sys
-import shutil
-from PIL import Image
+import sys, os
+import glob
 
 from colorama import Fore
-from requests import get
 
 colorama.init(autoreset=True)
 
@@ -18,108 +16,103 @@ def LocalChunksLoader():
     for i in aes['allPakFiles']:
         if i.startswith('FortniteGame/Content/Paks/'):
             path = i
-            print(Fore.YELLOW + f" [{time.strftime('%H:%M')}]" + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Loaded: " + Fore.GREEN + path)
+            pathname = (path.split('FortniteGame/Content/Paks/')[-1])
+            print(Fore.YELLOW + f" [{time.strftime('%H:%M')}]" + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Loaded: " + Fore.GREEN + pathname)
 
 def NewOutfitAssets():
     with open('datas/assets.json', 'r') as Icons:
         Icons = json.load(Icons)
-        for i in Icons:
-            if i.startswith('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Outfit/'):
-                if i.endswith('Bundle.uasset'):
-                    print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.RED + "Skipped " + Fore.CYAN + path)
-                else:    
-                    path = i
-                    print(Fore.YELLOW + f" [{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
-                    image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
-                    pathname = (path.split('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Outfit/')[-1])
-                    open(f'images/{pathname}.png', 'wb+').write(image.content)
+    for i in Icons:
+        if i.startswith('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Outfit/T-AthenaSoldiers'): 
+            path = i
+            print(Fore.YELLOW + f" [{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
+            image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
+            pathname = (path.split('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Outfit/')[-1])
+            open(f'images/{pathname}.png', 'wb+').write(image.content)
 
 def NewBackblingAssets():
     with open('datas/assets.json', 'r') as Icons:
         Icons = json.load(Icons)
-        for i in Icons:
-            if i.startswith('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Backpack/'):
-                path = i
-                print(Fore.YELLOW + f" [{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
-                image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
-                pathname = (path.split('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Backpack/')[-1])
-                open(f'images/{pathname}.png', 'wb+').write(image.content)
+    for i in Icons:
+        if i.startswith('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Backpack/'):
+            path = i
+            print(Fore.YELLOW + f" [{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
+            image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
+            pathname = (path.split('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Backpack/')[-1])
+            open(f'images/{pathname}.png', 'wb+').write(image.content)
 
 def NewPickaxeAssets():
     with open('datas/assets.json', 'r') as Icons:
         Icons = json.load(Icons)
-        for i in Icons:
-            if i.startswith('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Pickaxe/'):
-                path = i
-                print(Fore.YELLOW + f" [{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
-                image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
-                pathname = (path.split('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Pickaxe/')[-1])
-                open(f'images/{pathname}.png', 'wb+').write(image.content)
+    for i in Icons:
+        if i.startswith('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Pickaxe/'):
+            path = i
+            print(Fore.YELLOW + f" [{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
+            image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
+            pathname = (path.split('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Pickaxe/')[-1])
+            open(f'images/{pathname}.png', 'wb+').write(image.content)
 
 def NewGliderAssets():
     with open('datas/assets.json', 'r') as Icons:
         Icons = json.load(Icons)
-        for i in Icons:
-            if i.startswith('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Glider/'):
-                path = i
-                print(Fore.YELLOW + f" [{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
-                image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
-                pathname = (path.split('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Glider/')[-1])
-                open(f'images/{pathname}.png', 'wb+').write(image.content)
+    for i in Icons:
+        if i.startswith('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Glider/'):
+            path = i
+            print(Fore.YELLOW + f" [{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
+            image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
+            pathname = (path.split('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Glider/')[-1])
+            open(f'images/{pathname}.png', 'wb+').write(image.content)
 
 def NewEmoteAssets():
     with open('datas/assets.json', 'r') as Icons:
         Icons = json.load(Icons)
-        for i in Icons:
-            if i.startswith('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Emotes/'):
-                path = i
-                print(Fore.YELLOW + f" [{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
-                image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
-                pathname = (path.split('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Emotes/')[-1])
-                open(f'images/{pathname}.png', 'wb+').write(image.content)
+    for i in Icons:
+        if i.startswith('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Emotes/'):
+            path = i
+            print(Fore.YELLOW + f" [{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
+            image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
+            pathname = (path.split('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Emotes/')[-1])
+            open(f'images/{pathname}.png', 'wb+').write(image.content)
+
+def NewEmojiAssets():
+    with open('datas/assets.json', 'r') as Icons:
+        Icons = json.load(Icons)
+    for i in Icons:
+        if i.startswith('FortniteGame/Content/2dAssets/Emoji/Season18/'):
+            path = i
+            print(Fore.YELLOW + f" [{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
+            image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
+            pathname = (path.split('FortniteGame/Content/2dAssets/Emoji/Season18/')[-1])
+            open(f'images/{pathname}.png', 'wb+').write(image.content)                 
 
 def NewBundleAssets():
     with open('datas/assets.json', 'r') as Icons:
         Icons = json.load(Icons)
-        for i in Icons:
-            if i.startswith('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Outfit/'):
-                if i.endswith('Bundle.uasset'):
-                    path = i
-                    print(Fore.YELLOW + f" [{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
-                    image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
-                    pathname = (path.split('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Outfit/')[-1])
-                    open(f'images/{pathname}.png', 'wb+').write(image.content)
-                else:
-                    path = i
-                    print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.RED + "Skipped " + Fore.CYAN + path)    
+    for i in Icons:
+        if i.startswith('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Outfit/T-AthenaBundles'):
+            path = i
+            print(Fore.YELLOW + f" [{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
+            image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
+            pathname = (path.split('FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Outfit/')[-1])
+            open(f'images/{pathname}.png', 'wb+').write(image.content) 
 
 def NewWeaponAssets():
     with open('datas/assets.json', 'r') as Icons:
         Icons = json.load(Icons)
-        for i in Icons:
-            if i.startswith('FortniteGame/Plugins/GameFeatures/CorruptionGameplay/Content/Gameplay/Icons/'):
-                if i.endswith('-L.uasset'):
-                    path = i
-                    print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
-                    image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
-                    pathname = (path.split('FortniteGame/Plugins/GameFeatures/CorruptionGameplay/Content/Gameplay/Icons/')[-1])
-                    open(f'images/{pathname}.png', 'wb+').write(image.content)
-                else:
-                    path = i
-                    print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.RED + "Skipped " + Fore.CYAN + path)
-
-def NewComsuableAssets():
-    with open('datas/assets.json', 'r') as Icons:
-        Icons = json.load(Icons)
-        for i in Icons:
-            if i.startswith('FortniteGame/Content/Athena/Items/Consumables/PlaysetGrenade/'):
+    for i in Icons:
+        if i.startswith('FortniteGame/Plugins/GameFeatures/CorruptionGameplay/Content/Gameplay/Icons/'):
+            if i.endswith('-L.uasset'):
                 path = i
                 print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
                 image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
-                pathname = (path.split('FortniteGame/Content/Athena/Items/Consumables/PlaysetGrenade/')[-1])
-                open(f'images/{pathname}.png', 'wb+').write(image.content)                                    
+                pathname = (path.split('FortniteGame/Plugins/GameFeatures/CorruptionGameplay/Content/Gameplay/Icons/')[-1])
+                open(f'images/{pathname}.png', 'wb+').write(image.content)
+            else:
+                path = i
+                print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.RED + "Skipped " + Fore.CYAN + path)
 
-ask = input(f'What do you want to generate?\n(1) Generate New Outfits\n(2) Generate New Backblings\n(3) Generate New Pickaxes\n(4) Generate New Gliders\n(5) Generate New Emotes\n(6) Generate New Bundles\n(7) Generate New Weapons\n(8) Generate New Consumables\n(9) Generate All New Assets\n->>> ')
+print(f'What do you want to generate?\n' + Fore.CYAN + f'(1) Generate New Outfits\n(2) Generate New Backblings\n(3) Generate New Pickaxes\n(4) Generate New Gliders\n(5) Generate New Emotes and New Emojis\n(6) Generate New Bundles\n(7) Generate New Weapons\n' + Fore.LIGHTBLUE_EX + f'(8) Generate All New Assets\n' + Fore.LIGHTRED_EX + f'(9) Delete all images in images folder' + Fore.WHITE)
+ask = (input("->>> "))
 if ask == '1':
     start = time.time()
     print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.WHITE + "Data Generated - Inizialing...")
@@ -129,6 +122,8 @@ if ask == '1':
     print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Finished) " + Fore.WHITE + f"Data Generated in {round(end - start, 2)}")
     sleep(5)
     sys.exit()
+else:
+    print(Fore.RED + f'Invalid input...')
 if ask == '2':
     start = time.time()
     print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.WHITE + "Data Generated - Inizialing...")
@@ -161,10 +156,11 @@ if ask == '5':
     print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.WHITE + "Data Generated - Inizialing...")
     LocalChunksLoader()
     NewEmoteAssets()
+    NewEmojiAssets()
     end = time.time()
     print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Finished) " + Fore.WHITE + f"Data Generated in {round(end - start, 2)}")
     sleep(5)
-    sys.exit()
+    sys.exit()    
 if ask == '6':
     start = time.time()
     print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.WHITE + "Data Generated - Inizialing...")
@@ -182,26 +178,8 @@ if ask == '7':
     end = time.time()
     print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Finished) " + Fore.WHITE + f"Data Generated in {round(end - start, 2)}")
     sleep(5)
-    sys.exit()
+    sys.exit()                  
 if ask == '8':
-    start = time.time()
-    print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.WHITE + "Data Generated - Inizialing...")
-    LocalChunksLoader()
-    consumable = input('What is the folder name which contains consumable images?\n->>> ')
-    with open('datas/assets.json', 'r') as Icons:
-        Icons = json.load(Icons)
-        for i in Icons:
-            if i.startswith(f'FortniteGame/Content/Athena/Items/Consumables/{consumable}/'):
-                path = i
-                print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
-                image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
-                pathname = (path.split(f'FortniteGame/Content/Athena/Items/Consumables/{consumable}/')[-1])
-                open(f'images/{pathname}.png', 'wb+').write(image.content)
-                end = time.time()
-                print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Finished) " + Fore.WHITE + f"Data Generated in {round(end - start, 2)}")
-                sleep(5)
-                sys.exit()                  
-if ask == '9':
     start = time.time()
     print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.WHITE + "Data Generated - Inizialing...")
     LocalChunksLoader()
@@ -210,19 +188,15 @@ if ask == '9':
     NewPickaxeAssets()
     NewGliderAssets()
     NewEmoteAssets()
+    NewEmojiAssets()
     NewBundleAssets()
     NewWeaponAssets()
-    consumable = input('What is the folder name which contains consumable images?\n->>> ')
-    with open('datas/assets.json', 'r') as Icons:
-        Icons = json.load(Icons)
-        for i in Icons:
-            if i.startswith(f'FortniteGame/Content/Athena/Items/Consumables/{consumable}/'):
-                path = i
-                print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Info) " + Fore.BLUE + "Generating: " + Fore.GREEN + path)
-                image = requests.get(f'https://benbot.app/api/v1/exportAsset?path={path}')
-                pathname = (path.split(f'FortniteGame/Content/Athena/Items/Consumables/{consumable}/')[-1])
-                open(f'images/{pathname}.png', 'wb+').write(image.content)
-                end = time.time()
-                print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Finished) " + Fore.WHITE + f"Data Generated in {round(end - start, 2)}")
-                sleep(5)
-                sys.exit()     
+    end = time.time()
+    print(Fore.YELLOW + f"[{time.strftime('%H:%M')}] " + Fore.MAGENTA + "(Finished) " + Fore.WHITE + f"Data Generated in {round(end - start, 2)}")
+    sleep(5)
+    sys.exit()
+if ask == '9':
+    delete_files = glob.glob('images/*')
+    for file in delete_files:
+        os.remove(file)
+    print(Fore.GREEN + f'\nSuccessfully deleted all images in images folder\n')
